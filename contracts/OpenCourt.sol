c491d6a1771b9e6d05092ea063ae55d8476c1530
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2020-05-27 18:52:53 -0400

    Update OpenCourt.sol
    add `transferFrom` remote send function to basic token interface

--- contracts/OpenCourt.sol
+++ contracts/OpenCourt.sol
@@ -38,6 +38,7 @@ contract Context {
 interface IToken { // brief ERC-20 interface
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
 /*****************
